395a590a22c9e3e6d6cdf98efce136ca5f2deafe
ahmad <ahmadtarawneah@yahoo.com>
2024-01-14 09:12:09 +0300

    init

--- ticTacToe/Form1.Designer.cs
+++ ticTacToe/Form1.Designer.cs
@@ -31,9 +31,9 @@ namespace ticTacToe
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.A1 = new System.Windows.Forms.Button();
             this.A2 = new System.Windows.Forms.Button();
@@ -67,6 +67,20 @@ namespace ticTacToe
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // helpToolStripMenuItem
             // 
             this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -75,23 +89,10 @@ namespace ticTacToe
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.helpToolStripMenuItem.Text = "Help";
             // 
-            // newGameToolStripMenuItem
-            // 
-            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
-            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.newGameToolStripMenuItem.Text = "New Game";
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
             this.aboutToolStripMenuItem.Text = "About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
@@ -103,6 +104,7 @@ namespace ticTacToe
             this.A1.Size = new System.Drawing.Size(75, 75);
             this.A1.TabIndex = 1;
             this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button_click);
             // 
             // A2
             // 
@@ -112,6 +114,7 @@ namespace ticTacToe
             this.A2.Size = new System.Drawing.Size(75, 75);
             this.A2.TabIndex = 2;
             this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button_click);
             // 
             // A3
             // 
@@ -121,6 +124,7 @@ namespace ticTacToe
             this.A3.Size = new System.Drawing.Size(75, 75);
             this.A3.TabIndex = 3;
             this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button_click);
             // 
             // B1
             // 
@@ -130,6 +134,7 @@ namespace ticTacToe
             this.B1.Size = new System.Drawing.Size(75, 75);
             this.B1.TabIndex = 4;
             this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button_click);
             // 
             // B2
             // 
@@ -139,6 +144,7 @@ namespace ticTacToe
             this.B2.Size = new System.Drawing.Size(75, 75);
             this.B2.TabIndex = 5;
             this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button_click);
             // 
             // B3
             // 
@@ -148,6 +154,7 @@ namespace ticTacToe
             this.B3.Size = new System.Drawing.Size(75, 75);
             this.B3.TabIndex = 6;
             this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button_click);
             // 
             // C1
             // 
@@ -157,6 +164,7 @@ namespace ticTacToe
             this.C1.Size = new System.Drawing.Size(75, 75);
             this.C1.TabIndex = 7;
             this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button_click);
             // 
             // C2
             // 
@@ -166,6 +174,7 @@ namespace ticTacToe
             this.C2.Size = new System.Drawing.Size(75, 75);
             this.C2.TabIndex = 8;
             this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button_click);
             // 
             // C3
             // 
@@ -175,6 +184,7 @@ namespace ticTacToe
             this.C3.Size = new System.Drawing.Size(75, 75);
             this.C3.TabIndex = 9;
             this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.button_click);
             // 
             // Form1
             // 
@@ -195,6 +205,7 @@ namespace ticTacToe
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tic Tac Toe";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
